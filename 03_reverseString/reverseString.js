b4e1a3bb6a25cf85c5ceeff97102853a63615b98
Brad <bradgrensewich@gmail.com>
2022-03-03 16:48:45 -0500

    testing push from linus FS

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,6 @@
 const reverseString = function(str) {
     let newStr = "";
-    for(let i = str.length; i > 0; i++) {
+    for(let i = str.length; i > 0; i--) {
         newStr = newStr + str.charAt(i -1);
     }
     return newStr;
